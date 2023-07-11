850443a82cfe4b347d17631f78a36621cba271a0
Joe <Jhuntenburg@gmail.com>
2023-07-11 19:13:35 -0400

    more of part 2 complete

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,5 +1,8 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Classroom {
 
     Student[] students;
@@ -36,11 +39,58 @@ public class Classroom {
 
     public void addStudent(Student student){
         Student[] theArray = new Student[students.length + 1];
+        for (int i = 0; i < students.length; i++) {
+            theArray[i] = students[i] ;
+
+        }
         theArray[students.length-1] = student;
         this.students = theArray;
     }
 
     public void removeStudent(String firstName, String lastName){
+        ArrayList<Student> theArray = new ArrayList<>(Arrays.asList(students));
+        for (int i = 0; i < students.length; i++) {
+            Student student = students[i];
+           String fName = student.getFirstName();
+           String lName = student.getLastName();
+           if(fName.equals(firstName) && lName.equals(lastName)){
+               theArray.remove(student);
+           }
 
+        }
+        theArray.add(null);
+
+        this.students =theArray.toArray(new Student[students.length]);
     }
+
+    public Student[] getStudentsByScore(){
+        ArrayList<Student> sortingStudents = new ArrayList<>();
+        
+        for( Student s : students){
+            int idx = 0;
+            double currentStudentScore = s.getAverageExamScore();
+            double otherStudentScore;
+            
+            while (idx < sortingStudents.size()){
+                otherStudentScore =sortingStudents.get(idx).getAverageExamScore();
+                
+                if(currentStudentScore > otherStudentScore){
+                    break;
+                } else if (currentStudentScore == otherStudentScore) {
+                    if(s.getLastName().compareTo(sortingStudents.get(idx).getLastName()) > 0){
+                        break;
+                    } else if (s.getLastName().compareTo(sortingStudents.get(idx).getLastName()) == 0) {
+                        if(s.getFirstName().compareTo(sortingStudents.get(idx).getFirstName()) > 0){
+                            break;
+                        }
+                    }
+                }
+                idx++;
+            }
+            sortingStudents.add(idx,s);
+        }
+        return sortingStudents.toArray(new Student[students.length]);
+    }
+
+
 }
